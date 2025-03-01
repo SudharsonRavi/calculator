6d97330eb1a78cf03f78d99392b84c78cf032299
sudharson <sudharsonn03@gmail.com>
2025-03-01 14:15:56 +0530

    hot fix resolved division error

--- src/Calculator.java
+++ src/Calculator.java
@@ -20,7 +20,7 @@ public class Calculator {
             case '-': result = num1 - num2; break;
             case '*': result = num1 * num2; break;
             case '/': 
-                if (num2 ! = 0) {
+                if (num2 != 0) {
                     result = num1 / num2;
                 } else {
                     System.out.println("Error: Division by zero is not allowed.");
