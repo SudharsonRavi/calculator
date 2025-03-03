692979d5543227976153dec45739a28b6c2ea5e0
sudharson <sudharsonn03@gmail.com>
2025-03-03 11:18:32 +0530

    changes made

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Enter first number: ");
-        double num1 = scanner.nextDouble();
+        double num1 =   scanner.nextDouble();
         
         System.out.println("Enter an operator (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
